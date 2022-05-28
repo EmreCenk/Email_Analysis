7156582d1081473e709378ff2955467037e7dd61
Emre Cenk <emrecenk9@gmail.com>
2022-05-27 22:05:47 -0400

    made utility function to sort dictionary based on values

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 
 # Importing libraries
 from ParsingAndSavingEmails import count_all_emails
-print(count_all_emails())
+from utils import sort_dictionary_on_values
+
+email_count = count_all_emails()
+sorted = sort_dictionary_on_values(email_count)
+
+for name in sorted:
+    print(name, email_count[name])
---
+++ utils.py
@@ -0,0 +1,5 @@
+
+
+
+def sort_dictionary_on_values(to_sort):
+    return sorted(to_sort, key = lambda x: to_sort[x])
