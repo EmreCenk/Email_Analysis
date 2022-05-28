d7c3c063f678e7618f9a5a0d8872af56ee281e7c
Emre Cenk <emrecenk9@gmail.com>
2022-05-27 22:15:09 -0400

    moved around files and created risk for no reason

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 
 # Importing libraries
-from ParsingAndSavingEmails import count_all_emails
-from utils import sort_dictionary_on_values
+from src.EmailProcedures.ParsingAndSavingEmails import count_all_emails
+from src.EmailProcedures.utils import sort_dictionary_on_values
 
 email_count = count_all_emails()
 sorted = sort_dictionary_on_values(email_count)
